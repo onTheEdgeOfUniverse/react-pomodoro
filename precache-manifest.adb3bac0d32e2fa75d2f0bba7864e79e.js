bd2d9c5f61075ed18c542431cdb86bfd9c3c6a74
onTheEdgeOfUniverse <49761736+onTheEdgeOfUniverse@users.noreply.github.com>
2022-01-21 18:10:52 +0530

    Updates

---
+++ precache-manifest.adb3bac0d32e2fa75d2f0bba7864e79e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2cde679fd3f4c2650cac2a21345833",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/index.html"
+  },
+  {
+    "revision": "6c67f40ff9739166e87a",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/static/css/main.54c086ba.chunk.css"
+  },
+  {
+    "revision": "d06aa85e6cde01125914",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/static/js/2.218f4da8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/static/js/2.218f4da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c67f40ff9739166e87a",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/static/js/main.e9ccac6e.chunk.js"
+  },
+  {
+    "revision": "bb9b82a0b53c0a5e1f04",
+    "url": "/onTheEdgeOfUniverse.github.io/react-pomodor/static/js/runtime-main.4596c657.js"
+  }
+]);
